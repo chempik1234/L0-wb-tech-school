f0a8bd605dd4f4eda40dbbb51225d86fd2fd26f4
Danis <dandijar@yandex.ru>
2025-08-22 16:51:08 +0500

    fix(service): LRU cache & linked list fix after testing

--- order_service/pkg/linkedlist/list.go
+++ order_service/pkg/linkedlist/list.go
@@ -21,8 +21,15 @@ type LinkedList[ValueType any] struct {
 	mu     sync.Mutex
 }
 
-// ErrInvalidIndex describes an error when there is anything wrong with given index, e.g. length=0 or index<0
+// ErrInvalidIndex describes an error when index is incorrect
+//
+// If length is 0 then use ErrEmptyList
 var ErrInvalidIndex = errors.New("invalid index")
+
+// ErrEmptyList describes an error when trying to get and element but there mustn't be any
+//
+// Because length = 0
+var ErrEmptyList = errors.New("zero length")
 
 // NewLinkedList creates a new LinkedList with given ValueType, any ValueType is supported
 func NewLinkedList[ValueType any]() LinkedList[ValueType] {
@@ -43,27 +50,34 @@ func (l *LinkedList[ValueType]) Insert(data ValueType, index int) error {
 	newNode := &Node[ValueType]{data, nil}
 	currentLen := l.Len()
 
+	if index > currentLen {
+		return ErrInvalidIndex
+	}
+
 	if index == 0 {
-		if currentLen > 0 {
-			newNode.next = l.head
-		}
+		newNode.next = l.head
 		l.head = newNode
 	} else if index > 0 {
-		prev := l.head
-		for i := 0; i < index-1; i++ {
-			prev = prev.next
+		if index < currentLen {
+			prev := l.head
+			for i := 0; i < index-1; i++ {
+				prev = prev.next
+			}
+			if prev.next != nil {
+				newNode.next = prev.next
+			}
+			prev.next = newNode
+		} else if index == currentLen { // and it's certainly > 0, so there's a tail
+			l.tail.next = newNode
 		}
-		if prev.next != nil {
-			newNode.next = prev.next
-		}
-		prev.next = newNode
 	} else {
 		return ErrInvalidIndex
 	}
-	if currentLen == 0 {
-		l.tail = newNode
-	}
 	l.length++
+
+	if index == currentLen {
+		l.setTail(newNode)
+	}
 
 	return nil
 }
@@ -77,18 +91,23 @@ func (l *LinkedList[ValueType]) InsertLast(data ValueType) error {
 
 //region remove
 
+// RemoveAt removes element by given index
 func (l *LinkedList[ValueType]) RemoveAt(index int) error {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	return l.removeNodeAt(index)
 }
 
+// RemoveFirst removes first element
 func (l *LinkedList[ValueType]) RemoveFirst() error {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	return l.removeFirstNode()
 }
 
+// RemoveLast removes last element from the list
+//
+// Is tolerant to 0 length
 func (l *LinkedList[ValueType]) RemoveLast() error {
 	l.mu.Lock()
 	defer l.mu.Unlock()
@@ -96,33 +115,49 @@ func (l *LinkedList[ValueType]) RemoveLast() error {
 }
 
 func (l *LinkedList[ValueType]) removeNodeAt(index int) error {
+	length := l.Len()
+	if length == 0 {
+		return ErrEmptyList
+	}
+
 	if index == 0 {
 		return l.removeFirstNode()
 	}
 
-	if index < 0 || index >= l.Len() {
+	if index < 0 || index >= length {
 		return ErrInvalidIndex
 	}
 
-	var prevElem *Node[ValueType]
-	currentElem := l.head
-	for currentElem.next != nil {
-		prevElem = currentElem
-		currentElem = currentElem.next
+	prevElem := l.head
+	for i := 0; i < index-1; i++ {
+		prevElem = prevElem.next
 	}
 
-	prevElem.next = nil
+	deletedElem := prevElem.next
+	if deletedElem != nil {
+		prevElem.next = deletedElem.next
+	} else {
+		prevElem.next = nil
+	}
+
+	if index == l.Len()-1 {
+		l.setTail(prevElem)
+	}
 	l.length--
 	return nil
 }
 
 func (l *LinkedList[ValueType]) removeFirstNode() error {
-	if l.Len() > 0 {
-		l.head = l.head.next
-		l.length--
-	} else {
-		return ErrInvalidIndex
+	length := l.Len()
+	if length == 0 {
+		return ErrEmptyList
 	}
+
+	l.head = l.head.next
+	l.length--
+
+	l.recalculateTail()
+
 	return nil
 }
 
@@ -138,6 +173,9 @@ func (l *LinkedList[ValueType]) GetAt(index int) (ValueType, error) {
 	if err != nil {
 		return *new(ValueType), err
 	}
+	if node == nil {
+		return *new(ValueType), ErrEmptyList
+	}
 	return node.value, nil
 }
 
@@ -148,6 +186,9 @@ func (l *LinkedList[ValueType]) GetFirst() (ValueType, error) {
 	node, err := l.getFirstNode()
 	if err != nil {
 		return *new(ValueType), err
+	}
+	if node == nil {
+		return *new(ValueType), ErrEmptyList
 	}
 	return node.value, nil
 }
@@ -165,7 +206,7 @@ func (l *LinkedList[ValueType]) GetLast() (ValueType, error) {
 
 func (l *LinkedList[ValueType]) getFirstNode() (*Node[ValueType], error) {
 	if l.Len() == 0 {
-		return nil, ErrInvalidIndex
+		return nil, ErrEmptyList
 	}
 	return l.head, nil
 }
@@ -173,7 +214,7 @@ func (l *LinkedList[ValueType]) getFirstNode() (*Node[ValueType], error) {
 func (l *LinkedList[ValueType]) getNodeAt(index int) (*Node[ValueType], error) {
 	length := l.Len()
 	if length == 0 {
-		return nil, ErrInvalidIndex
+		return nil, ErrEmptyList
 	}
 
 	if index == 0 {
@@ -193,7 +234,7 @@ func (l *LinkedList[ValueType]) getNodeAt(index int) (*Node[ValueType], error) {
 
 func (l *LinkedList[ValueType]) getLastNode() (*Node[ValueType], error) {
 	if l.Len() == 0 {
-		return nil, ErrInvalidIndex
+		return nil, ErrEmptyList
 	}
 	return l.tail, nil
 }
@@ -225,7 +266,7 @@ func (l *LinkedList[ValueType]) MoveToFirst(from int) error {
 		// by this moment we're sure it exists
 		nodeToMove := prevNode.next
 
-		nextNode := prevNode.next
+		nextNode := nodeToMove.next
 
 		currentHead := l.head
 
@@ -234,6 +275,56 @@ func (l *LinkedList[ValueType]) MoveToFirst(from int) error {
 		l.head = nodeToMove
 		nodeToMove.next = currentHead
 		prevNode.next = nextNode
+
+		// find tail
+		l.recalculateTail()
 	}
 	return nil
+}
+
+// GetIndex tries to find first entry of element
+//
+// return index -1 if not found
+//
+// required custom compareFunc because ValueType is not always comparable
+//
+// compareFunc must return true if values is equal, else false
+func (l *LinkedList[ValueType]) GetIndex(
+	item ValueType,
+	compareFunc func(item1 ValueType, item2 ValueType) bool,
+) (int, error) {
+	if l.Len() == 0 {
+		return -1, ErrEmptyList
+	}
+	elem := l.head
+	for i := 0; i < l.Len(); i++ {
+		if compareFunc(elem.value, item) {
+			return i, nil
+		}
+		elem = elem.next
+	}
+	return -1, nil
+}
+
+// GetAll returns a slice of ValueType, in same order as stored
+func (l *LinkedList[ValueType]) GetAll() []ValueType {
+	result := make([]ValueType, l.Len())
+	currentElem := l.head
+	for i := 0; i < l.Len(); i++ {
+		result[i] = currentElem.value
+		currentElem = currentElem.next
+	}
+	return result
+}
+
+func (l *LinkedList[ValueType]) recalculateTail() {
+	currentElem := l.head
+	for i := 0; i < l.Len()-1; i++ {
+		currentElem = currentElem.next
+	}
+	l.setTail(currentElem)
+}
+
+func (l *LinkedList[ValueType]) setTail(newTail *Node[ValueType]) {
+	l.tail = newTail
 }
--- order_service/pkg/pkgports/adapters/cache/lru/in_memory.go
+++ order_service/pkg/pkgports/adapters/cache/lru/in_memory.go
@@ -2,6 +2,7 @@ package lru
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"go.uber.org/zap"
 	"order_service/pkg/linkedlist"
@@ -9,10 +10,8 @@ import (
 	"sync"
 )
 
-type lruKey[Value any] struct {
-	Value Value
-	Index int
-}
+// ErrUnexpectedLinkedListBehaviour describes an error when linked list works wrong way
+var ErrUnexpectedLinkedListBehaviour = errors.New("unexpected linked list behaviour")
 
 // CacheLRUInMemory saves up to N Values and LRU algorithm and in-memory map storage
 //
@@ -20,7 +19,7 @@ type lruKey[Value any] struct {
 //
 // It uses sync.RWMutex because there are going to be many read operations from the web
 type CacheLRUInMemory[Key comparable, Value any] struct {
-	data     map[Key]lruKey[Value]
+	data     map[Key]Value
 	keysList linkedlist.LinkedList[Key]
 	mu       sync.RWMutex
 	cap      int
@@ -30,23 +29,32 @@ type CacheLRUInMemory[Key comparable, Value any] struct {
 // A) store key index in keysList (in a separate map or in the data field)
 // B) don't store key index and look it up every time I GET an element (LRU moves it to the top)
 //
-// option A: after every SET, when data is inserted into linked list, we have to loop through N values and update index
+// option A:
+//   1. after every SET, when data is inserted into linked list, we have to loop through N values and update index
+//   2. after every GET, when key is moved to top, we have to loop through N-1 values and update index
 // option B: after every GET, when data is retrieved from the list, we have to loop through N values to find the index
 //
-// GET happens more often, so we choose option A
+// we choose option B
 
 // NewCacheLRUInMemory creates a new CacheLRUInMemory with given capacity and key/value types
 //
 // Example: myCache := NewCacheLRUInMemory[string, myStruct](myCapacity)
 func NewCacheLRUInMemory[Key comparable, Value any](cacheCapacity int) *CacheLRUInMemory[Key, Value] {
 	return &CacheLRUInMemory[Key, Value]{
-		data:     make(map[Key]lruKey[Value]),
+		data:     make(map[Key]Value),
 		keysList: linkedlist.NewLinkedList[Key](),
 		cap:      cacheCapacity,
 	}
 }
 
+// GetCapacity returns read-only value of CacheLRUInMemory capacity
+func (c *CacheLRUInMemory[Key, Value]) GetCapacity() int {
+	return c.cap
+}
+
 // Get tries to get an item by key, logs on miss
+//
+// It also moves read item to the top (if able)
 func (c *CacheLRUInMemory[Key, Value]) Get(ctx context.Context, key Key) (Value, bool, error) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
@@ -54,15 +62,30 @@ func (c *CacheLRUInMemory[Key, Value]) Get(ctx context.Context, key Key) (Value,
 	value, ok := c.data[key]
 
 	if ok {
-		err := c.keysList.MoveToFirst(value.Index)
+		index, err := c.keysList.GetIndex(key, func(a, b Key) bool { return a == b })
 		if err != nil {
-			return *new(Value), false, fmt.Errorf("error while putting element to top: %w", err)
+			if errors.Is(err, linkedlist.ErrEmptyList) {
+				return *new(Value), false, fmt.Errorf("%w: key is stored in data, but keys linked list is empty",
+					ErrUnexpectedLinkedListBehaviour)
+			} else {
+				// not going to occur
+				return value, false, fmt.Errorf("error getting index of read key: %w", err)
+			}
+		}
+		if index != -1 {
+			err = c.keysList.MoveToFirst(index)
+			if err != nil {
+				return *new(Value), false, fmt.Errorf("error while putting element to top: %w", err)
+			}
+		} else {
+			return *new(Value), false, fmt.Errorf("%w: key \"%v\" is stored in data, but not in non-empty linked list",
+				ErrUnexpectedLinkedListBehaviour, key)
 		}
 	} else {
 		logger.GetOrCreateLoggerFromCtx(ctx).Info(ctx, "in-memory LRU cache miss", zap.Any("key", key))
 	}
 
-	return value.Value, ok, nil
+	return value, ok, nil
 }
 
 // Set saves the value
@@ -72,25 +95,58 @@ func (c *CacheLRUInMemory[Key, Value]) Set(ctx context.Context, key Key, value V
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	// remove value if we're out of space
-	if c.keysList.Len() > c.cap {
-		keyToDelete, err := c.keysList.GetLast()
+	index, err := c.keysList.GetIndex(key, func(a, b Key) bool { return a == b })
+	if err != nil && !errors.Is(err, linkedlist.ErrEmptyList) {
+		return fmt.Errorf("error trying to get existing entry of set key: %w", err)
+	}
+	if index != -1 {
+		err = c.keysList.RemoveAt(index)
 		if err != nil {
-			return fmt.Errorf("error while getting last key index: %w", err)
-		}
-
-		delete(c.data, keyToDelete)
-		err = c.keysList.RemoveLast()
-		if err != nil {
-			return fmt.Errorf("error while removing last key index: %w", err)
+			return fmt.Errorf("error trying to remove existing entry of set key: %w", err)
 		}
 	}
 
-	err := c.keysList.Insert(key, 0)
+	err = c.keysList.Insert(key, 0)
 	if err != nil {
 		return fmt.Errorf("error inserting key in list: %w", err)
 	}
 
-	c.data[key] = lruKey[Value]{Index: 0, Value: value}
+	c.data[key] = value
+
+	// remove value if we're out of space
+	if c.keysList.Len() > c.cap {
+		var keyToDelete Key
+		keyToDelete, err = c.keysList.GetLast()
+
+		if err != nil {
+			if !errors.Is(err, linkedlist.ErrEmptyList) {
+				return fmt.Errorf("error while getting last key index: %w", err)
+			}
+		} else {
+			err = c.keysList.RemoveLast()
+			if err != nil {
+				return fmt.Errorf("error while removing last key index: %w", err)
+			}
+
+			logger.GetLoggerFromCtx(ctx).Info(ctx, "cache overflow, erased a value",
+				zap.Any("key", keyToDelete), zap.Int("length", c.keysList.Len()),
+				zap.Int("capacity", c.GetCapacity()))
+
+			delete(c.data, keyToDelete)
+		}
+	}
+
 	return nil
+}
+
+func (c *CacheLRUInMemory[Key, Value]) GetKeysOrder() []Key {
+	return c.keysList.GetAll()
+}
+
+func (c *CacheLRUInMemory[Key, Value]) MostUsedKey() (Key, error) {
+	return c.keysList.GetFirst()
+}
+
+func (c *CacheLRUInMemory[Key, Value]) LeastUsedKey() (Key, error) {
+	return c.keysList.GetLast()
 }
